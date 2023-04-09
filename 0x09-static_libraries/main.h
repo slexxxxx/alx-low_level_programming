a31ba2263c2ebecf0e8822af25dac02574690777
slexxxxx <kmustapha2843@gmail.com>
2023-04-09 07:42:22 -0700

    1st task

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
@@ -11,3 +22,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+
+#endif
