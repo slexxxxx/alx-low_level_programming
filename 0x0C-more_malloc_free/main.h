43305a19eceae0b76f0347e99facfe50601476d1
slexxxxx <kmustapha2843@gmail.com>
2023-04-13 07:28:16 -0700

    1st task

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _strlen(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
+#endif /* MAIN_H */
